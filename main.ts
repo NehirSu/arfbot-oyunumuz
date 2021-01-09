3cf8856a9ab04420d76ba8733a9fbdefd432f615
NehirSu <77208864+NehirSu@users.noreply.github.com>
2021-01-09 22:14:56 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+scene.setBackgroundColor(3)
+let mySprite = sprites.create(img`
+    . . . 2 . 2 . . . . 
+    . . a a a a a . . . 
+    . . a 8 a 8 a . . . 
+    . . 3 a a a 3 . . . 
+    . . a 8 8 8 a . . . 
+    . 2 a a a a a 2 . . 
+    . . 7 7 7 7 7 . . . 
+    . . . 2 . 2 . . . . 
+    . . 5 5 . 5 5 . . . 
+    . . . . . . . . . . 
+    `, SpriteKind.Player)
